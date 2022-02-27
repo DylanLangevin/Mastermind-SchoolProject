e9d1582bec5a0a7c4c4ec862d6c01087d4197b5e
DylanLangevin <dylan.lgvn@gmail.com>
2022-02-27 17:18:28 +0100

    version apres clean du code

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const colorsList = ["b", "r", "v", "j", "o"]
+const colorsList = [
+    "b",
+    "r", 
+    "v", 
+    "j", 
+    "o"
+]
 const redButton = document.getElementById("red-btn")
 const blueButton = document.getElementById("blue-btn")
 const greenButton = document.getElementById("green-btn")
@@ -9,91 +15,98 @@ const vldBtn = document.getElementById('validate-btn')
 const refreshBtn = document.getElementById('refresh-btn')
 const roundNumber = document.getElementById('round-circle')
 
+let userArray = []
+let r = 1
 
-let testArray = []
-r = 1
 
 // Fonction qui génère 4 couleurs parmis une liste de max couleurs
 function randomColorsGenerator(max, list) {
+
     let colorsSelectedList = []
     for (i=0;i<4;i++) {
+
         let randomIndex = Math.floor(Math.random() * max);
         colorsSelectedList.push(list[randomIndex])
     }
     return colorsSelectedList
 }
+
 // Fonction qui vérifie les couleurs bien et mal placée, et qui doit retourner la liste d'indice
-function verification(userList) {
+function verification(userArr) {
+
     let goodSpotGoodColor = 0
     let badSpotGoodColor = 0
+
+    // Copie de la séquence ordinateur à deviner
     let copyOfColorsSelectedList = colorsSelectedList.slice();
+
     // Boucle identifiant le nombre de couleur bien placée
     let i = 0
-    while (i < userList.length) {
-        if (userList[i] == copyOfColorsSelectedList[i] ){
+    while (i < userArr.length) {
+
+        if (userArr[i] == copyOfColorsSelectedList[i] ){
+
             goodSpotGoodColor++;
             copyOfColorsSelectedList.splice(i,1)
-            userList.splice(i,1)
+            userArr.splice(i,1)
+
         } else {
+
             i++;
         }
     }
-    console.log("Couleurs bien placées :",goodSpotGoodColor);
-    console.log(copyOfColorsSelectedList, "IA");
-    console.log(userList,"MOI");
-    // Boucle identifiant le nombre de couleur mal placée
+
+    // Boucle identifiant le nombre de couleur mal placée, si indexOf retourne un nombre positif, alors une couleur existe, mais est mal placée.
     i = 0
-    while (i < userList.length ) {
-        const positionColor = copyOfColorsSelectedList.indexOf(userList[i])
+    while (i < userArr.length) {
+        const positionColor = copyOfColorsSelectedList.indexOf(userArr[i])
 
-        if(positionColor > -1) {
+        if (positionColor > -1) {
+
             badSpotGoodColor++;
-
-            userList.splice(i, 1)
+            userArr.splice(i, 1)
             copyOfColorsSelectedList.splice(positionColor, 1)
+
         } else {
             i++;
         }
     }
 
-    // CODE QUI COLORE LES POINTS D'INDICE ------------------------------------
+    // Boucle colorant les points d'indice sur l'interface 
     amountOfColors = goodSpotGoodColor + badSpotGoodColor
-    j=1
-    while (j<=amountOfColors) {
+    j = 1
+    while (j <= amountOfColors) {
+
         for (rouge = 1; rouge <= goodSpotGoodColor; rouge++) {
-            console.log("ROUGE");
-            console.log(document.querySelector(`#indice-line-${r} .indice:nth-child(${j})`));
+
             document.querySelector(`#indice-line-${r} .indice:nth-child(${j})`).style.background = "red";
             j++;
         }
 
-
         for (blanc = 1;blanc <= badSpotGoodColor; blanc++){
-            console.log("BLANC");
-            console.log(document.querySelector(`#indice-line-${r} .indice:nth-child(${j})`));
+
             document.querySelector(`#indice-line-${r} .indice:nth-child(${j})`).style.background = "white";
             j++;
         }   
     }
-    // -----------------------------------------------------
-    console.log("Rouge : ", goodSpotGoodColor);
-    console.log("Blanc : ", badSpotGoodColor);
     return goodSpotGoodColor
 }
-// Fonction colorant une cercle, en fonction de la couleur paramétrée, et de la taille d'un tableau mis a jour à chaque utilisation de cette même fonction
+
+// Fonction colorant un cercle, en fonction de la couleur cliquée
 function setColor(colorLetter) {
 
-    if (testArray.length >= 4) {
-        console.log("oui");
-    } else {
-        testArray.push(`${colorLetter}`)
-        for (i = 1; i <= testArray.length; i++) {
-            document.querySelector(`#line-${r} .colors:nth-child(${i})`).classList.add(testArray[i-1])
+    if (userArray.length < 4) {
+
+        userArray.push(`${colorLetter}`)
+        for (i = 1; i <= userArray.length; i++) {
+            
+            document.querySelector(`#line-${r} .colors:nth-child(${i})`).classList.add(userArray[i-1])
         }
+
     }
-    console.log(testArray);
 }
-// ------------------ Fonction est appelé a la victoire, ou a la défaite, affichant les couleurs de l'ordinateur, donnant la possibilité de rejouer (rafraichir la page) et rendant impossible de cliquer sur les boutons  
+
+// Fonction appelé a la victoire, ou a la défaite, affichant les couleurs de l'ordinateur, donnant la possibilité de rejouer (rafraichir la page) et rendant impossible de cliquer sur les boutons  
 function showResult(win,text) {
 
         let resultDiv = document.createElement("div")
@@ -114,9 +127,12 @@ function showResult(win,text) {
         let resultText = document.createElement('span')
         resultText.style.fontSize = "300%"
         resultText.style.backgroundColor = "transparent"
+
         if (win) {
+
             resultText.style.color="rgb(9, 199, 9)"
         } else {
+
             resultText.style.color="red"
         }
         
@@ -136,22 +152,21 @@ function showResult(win,text) {
 
         resultDiv.appendChild(resultText);
         resultDiv.appendChild(seqOrdi);
+        // Boucle qui affiche les couleurs de l'ordinateur
         for (i=0; i<4; i++) {
+
             let colorsOrdi = document.createElement("div")
             colorsOrdi.classList.add("colors")
             colorsOrdi.classList.add(colorsSelectedList[i])
             seqOrdi.appendChild(colorsOrdi)
         }
 
-
-
         document.body.appendChild(resultDiv)
         resultDiv.style.boxShadow="10px 10px 100000px grey"
 
-
-
 }
 
+//  Au click de l'une d'un de ces boutons, on appelle la fonction setColor 
 redButton.onclick = function(){setColor('r',r);};
 blueButton.onclick = function(){setColor('b',r);};
 greenButton.onclick = function(){setColor('v',r);};
@@ -161,20 +176,23 @@ orangeButton.onclick = function(){setColor('o',r);};
 // Vide le tableau au click du bouton reset
 resetButton.addEventListener('click', () => {
     console.log("reset");
-    testArray = []
+    userArray = []
 
     for(boule=1;boule<5;boule++) {
+
         document.querySelector(`#line-${r} .colors:nth-child(${boule})`).className ="colors"
     }
 })
 
 vldBtn.onclick = function() {
-    if (testArray.length != 4) {
+    if (userArray.length != 4) {
         alert('Ta séquence de couleur est incomplète')
     } else {
-        goodSpotGoodColor = verification(testArray)
+        goodSpotGoodColor = verification(userArray)
         if (goodSpotGoodColor == 4 ){
+
             showResult(true,"GG")
+            // Modification du bouton "Rejouer" pour qu'il prenne toute la place, et du bouton "Valider" pour qu'il disparaisse
             refreshBtn.style.width = '100%'
             refreshBtn.style.transition = '1s'
             refreshBtn.style.boxShadow = '-2px -2px 5px white'
@@ -183,11 +201,12 @@ vldBtn.onclick = function() {
             vldBtn.textContent=''
             vldBtn.style.transition = '0.5s'
         } else {
+
             r += 1
 
             roundNumber.innerHTML = `${r}`
 
-            testArray = []
+            userArray = []
             
             if (r == 11) {
  
@@ -202,23 +221,16 @@ vldBtn.onclick = function() {
             }
             document.querySelector(`#line-${r}`).style.border = "1px white solid" 
             document.querySelector(`#line-${r-1}`).style.border = "0px" 
-            
             return r
         }
-        // sinon --> code en bas
     }
-    
-
 }
+
 refreshBtn.onclick = function() {
+
     location.reload();
 }
+
 const colorsSelectedList = randomColorsGenerator(colorsList.length, colorsList)
-console.log(colorsSelectedList, "IA");
+
 document.querySelector(`#line-${r}`).style.border = "1px white solid" 
-
-
-
-
-
-
